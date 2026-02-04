005d7c4adf782d9640082570cd72b76241a42838
Bilous Oleg <olezhka82802@gmail.com>
2026-02-04 12:32:04 +0200

    Add security and validation to classes

--- assets/js/animal.js
+++ assets/js/animal.js
@@ -1,6 +1,10 @@
 class Animal {
   // Не знав, що додати в якості властивостей, тому придумав від себе
   constructor(age, mood) {
+    // Унеможливив створення екземпляру абстрактного класу
+    if (new.target === Animal) {
+      throw new ReferenceError('cannot create instance of abstract class');
+    }
     this.age = age;
     this.mood = mood;
   }
@@ -9,6 +13,32 @@ class Animal {
   }
   growl() {
     console.log('Грррррр!');
+  }
+
+  set age(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('age must be a number value');
+    } else if (!Number.isSafeInteger(value) || value < 0) {
+      throw new RangeError('age must be a safe, non-negative integer');
+    }
+    this._age = value;
+  }
+
+  get age() {
+    return this._age;
+  }
+
+  set mood(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('mood must be a string value');
+    } else if (value.trim().length === 0) {
+      throw new Error('length of mood value must be greater than 0');
+    }
+    this._mood = value.trim();
+  }
+
+  get mood() {
+    return this._mood;
   }
 }
 
@@ -23,6 +53,17 @@ class Wolf extends Animal {
   }
   growl() {
     console.log(this.mood === 'peaceful' ? 'Ррр!' : 'Грррррррр!!!!');
+  }
+
+  set isAlpha(value) {
+    if (typeof value !== 'boolean') {
+      throw new TypeError('isAlpha must be a boolean value');
+    }
+    this._isAlpha = value;
+  }
+
+  get isAlpha() {
+    return this._isAlpha;
   }
 }
 
